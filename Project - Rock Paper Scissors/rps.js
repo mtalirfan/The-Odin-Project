c1e32b2368ed62ce06d797762ca93c329b8a12ca
mtalirfan <mtalirfan@gmail.com>
2024-07-06 16:47:16 +0500

    Add scoring to Project - Rock Paper Scissors
    Clean unused test code

--- Project - Rock Paper Scissors/rps.js
+++ Project - Rock Paper Scissors/rps.js
@@ -1,20 +1,25 @@
 function playRound(humanChoice, computerChoice) {
-  // your code here!
   console.log(`You chose ${humanChoice}`);
   console.log(`Computer chose ${computerChoice}`);
   if (humanChoice === computerChoice) {
     console.log("It's a tie!");
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore++;
     console.log("You win!");
   } else if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore++;
     console.log("You lose!");
   } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore++;
     console.log("You win!");
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore++;
     console.log("You lose!");
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore++;
     console.log("You win!");
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore++;
     console.log("You lose!");
   } else {
     console.log("Invalid input. Please try again.");
@@ -23,6 +28,9 @@ function playRound(humanChoice, computerChoice) {
 
 function getHumanChoice() {
   let choice = prompt("Enter your choice: rock, paper, or scissors");
+  if (choice == "") {
+    choice = "rock";
+  }
   try {
     choice = choice.toLowerCase();
   } catch (error) {
@@ -33,12 +41,26 @@ function getHumanChoice() {
 
 function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
-  let choice = choices[Math.floor(Math.random() * choices.length)];
+  let choice = choices[Math.floor(Math.random() * choices.length)]; // get random index 0, 1, 2
   return choice;
 }
+
+let humanScore = 0;
+let computerScore = 0;
 
 for (let index = 0; index < 5; index++) {
   const humanSelection = getHumanChoice();
   const computerSelection = getComputerChoice();
   playRound(humanSelection, computerSelection);
+}
+
+console.log(`Your score is ${humanScore}`);
+console.log(`Computer score is ${computerScore}`);
+
+if (humanScore === computerScore) {
+  console.log("It's a tie!");
+} else if (humanScore > computerScore) {
+  console.log("You win the game!");
+} else if (humanScore < computerScore) {
+  console.log("You lose the game!");
 }
