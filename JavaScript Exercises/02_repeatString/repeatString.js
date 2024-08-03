0916ce777701818e7ee70e6ecff3e428ba1e5b27
mtalirfan <mtalirfan@gmail.com>
2024-08-03 16:20:23 +0500

    Add JavaScript Exercises, Complete 1-3
    refreshing JS knowledge to continue further

---
+++ JavaScript Exercises/02_repeatString/repeatString.js
@@ -0,0 +1,11 @@
+const repeatString = function (string, times) {
+  if (times < 0) return "ERROR";
+  let result = "";
+  for (let i = 0; i < times; i++) {
+    result += string;
+  }
+  return result;
+};
+
+// Do not edit below this line
+module.exports = repeatString;
